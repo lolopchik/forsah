952620b2cfbc6c0402b68c4d29addf7b2914de79
lolopchik <147331169+lolopchik@users.noreply.github.com>
2023-11-18 18:21:11 +0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let button = document.querySelector(`#svet`);
+let body = document.querySelector(`body`)
+button.addEventListener(`click`, function() {
+    body.classList.toggle(`svet`)
+});
+
+
+
+let ludiNode = document.querySelector(`.ludi`);
+let filNode = document.querySelector(`.ludi`);
+let gameNode = document.querySelector(`.ludi`);
+let musiNode = document.querySelector(`.ludi`);
+
+
+l
